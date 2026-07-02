5c6b017c8ec4d8a1ac5cbb642116ac736303b98b
Umesh <majjada.umesh.min21@itbhu.ac.in>
2026-07-02 15:04:14 +0530

    test: add unit and controller tests

--- backend/route-optimization-service/src/test/java/com/umesh/route_optimization_service/RouteOptimizationServiceApplicationTests.java
+++ backend/route-optimization-service/src/test/java/com/umesh/route_optimization_service/RouteOptimizationServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.umesh.route_optimization_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RouteOptimizationServiceApplicationTests {
 
 	@Test
